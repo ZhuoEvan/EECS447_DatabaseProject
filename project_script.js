4656f6986a69c2805124dc5d4e2f9e94168c6d4a
ZhuoEvan <156927177+ZhuoEvan@users.noreply.github.com>
2026-04-09 10:44:41 -0500

    created files

---
+++ project_script.js
@@ -0,0 +1,8 @@
+/* ===[ SECTION 01 ]=================================== *
+ * File Name: project_script.js                         *
+ * 
+ * Creation Date: 04/09/2026                            *
+ * Last Modification Date: 04/09/2026                   *
+ * ==================================================== */
+
+//MIGHT NOT USE!
