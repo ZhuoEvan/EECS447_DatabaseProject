3515a850b0ce996d0d4ec56ba494c825679d1a6f
ZhuoEvan <156927177+ZhuoEvan@users.noreply.github.com>
2026-04-22 12:29:15 -0500

    its one php file now

--- project_script.js
+++
@@ -1,8 +0,0 @@
-/* ===[ SECTION 01 ]=================================== *
- * File Name: project_script.js                         *
- * 
- * Creation Date: 04/09/2026                            *
- * Last Modification Date: 04/09/2026                   *
- * ==================================================== */
-
-//MIGHT NOT USE!
